625b913ced0c1f8752407224ff27ecebfc116dcb
Fabian Holzer <fabian.holzer.lists@gmail.com>
2023-09-11 12:14:11 +0200

    Enable to create topic based table of contents

--- plugins/table-of-contents.js
+++ plugins/table-of-contents.js
@@ -7,16 +7,31 @@ export default async function tableOfContents({
     pluginParams,
     pluginElement,
 }) {
-    const table = 
+    const table =
         templateDom.window.document.getElementById(pluginParams.target);
 
-    const isListed = page => page.category === pluginParams.category && page.isPublished;
+    const isListedCategory = page => page.category === pluginParams.category && page.isPublished;
+
+    let listedTopics = (pluginParams.topics || "").split(",").map(t => t.trim()).filter(t => t);
+
+    const isListedTopic = page => {
+        return page.isPublished
+            && listedTopics.some(listedTopic => page.topics.includes(listedTopic));
+    }
+
+    let isListed = () => false;
+
+    if (pluginParams.category) {
+        isListed = isListedCategory;
+    } else if (pluginParams.topics) {
+        isListed = isListedTopic;
+    }
 
     const reverseChronologically =
         (a, b) => b.publicationDate.localeCompare(a.publicationDate)
 
     const toRow = page => `<tr><td>${page.publicationDate}</td><td><a href="${page.fullQualifiedURL}">${page.title}</a></td></tr>`;
-    
+
     const content =
         metamodel.pages
             .filter(isListed)
@@ -25,7 +40,13 @@ export default async function tableOfContents({
             .join("");
 
     const rows = [...htmlToElements(content, templateDom.window.document)];
-    const tbody = table.getElementsByTagName("tbody").item(0);
+
+    let tbody = table.getElementsByTagName("tbody").item(0);
+
+    if (!tbody) {
+        tbody = templateDom.window.document.createElement("tbody");
+        table.appendChild(tbody);
+    }
 
     for (let row of rows) {
         tbody.appendChild(row);
