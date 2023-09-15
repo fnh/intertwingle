2958947a5591fa28b77a61d0c9ff4215bfecdbb5
Fabian Holzer <fabian.holzer.lists@gmail.com>
2023-09-15 09:49:46 +0200

    Add last update plugin

---
+++ plugins/last-update.js
@@ -0,0 +1,22 @@
+export default async function lastUpdatedPage({
+    templateDom,
+    page,
+    metamodel,
+    pluginParams,
+    pluginElement,
+}) {
+    const reverseChronologically =
+        (a, b) => b.publicationDate?.localeCompare(a.publicationDate)
+
+    let [latest] =
+        metamodel.pages
+            .filter(page => page.isPublished && page.publicationDate)
+            .sort(reverseChronologically);
+
+    const path = new URL(latest.fullQualifiedURL).pathname;
+    let content =
+        `<a href="${path}">${latest.title}</a> (<time datetime="${page.publicationDate}">${latest.publicationDate}</time>)`
+
+    pluginElement.insertAdjacentHTML("afterend", content);
+    pluginElement.remove();
+}
