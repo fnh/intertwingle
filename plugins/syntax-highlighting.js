b0a85ee345b673861816430630687005b7f16e43
Fabian Holzer <fabian.holzer.lists@gmail.com>
2023-09-20 20:56:09 +0200

    Add syntax highlighting with prismjs

---
+++ plugins/syntax-highlighting.js
@@ -0,0 +1,36 @@
+import prism from 'prismjs';
+import { readFile } from "node:fs/promises";
+import path from "path";
+
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+const { highlight, languages } = prism;
+
+export default async function example({
+    templateDom,
+    page,
+    metamodel,
+    pluginParams,
+    pluginElement,
+}) {
+    const code = pluginElement.innerHTML;
+    const language = pluginElement.language || "javascript";
+
+    const highlightedCode =
+        `<code><pre>${highlight(code, languages[language], language)}</pre></code>`;
+
+    let document = templateDom.window.document;
+
+    // inject style
+    const pathToCss = path.join(__dirname, "../node_modules/prismjs/themes/prism.min.css");
+    const styles = await readFile(pathToCss, { encoding: "utf-8" });
+    const prismMinStyles = document.createElement("style");
+    prismMinStyles.innerHTML = styles;
+    document.head.appendChild(prismMinStyles);
+
+    pluginElement.insertAdjacentHTML("afterend", highlightedCode);
+
+    pluginElement.remove();
+}
