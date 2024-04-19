ac04eae089e0bbfc73d118182e48b58ad33d48c4
Fabian Holzer <fabian.holzer.lists@gmail.com>
2024-04-19 23:49:14 +0200

    Plugin to create link to previous page of category

---
+++ plugins/previous-page.js
@@ -0,0 +1,36 @@
+export default async function addPreviousPage({
+    templateDom,
+    page,
+    metamodel,
+    pluginParams,
+    pluginElement,
+}) {
+
+    let targetSelector = pluginParams.target || "article";
+
+    let linkText = pluginParams["link-text"];
+
+    let pages = metamodel.pages.filter(p => p.category === page.category && p.isPublished && p.publicationDate);
+
+    let byDateAsc = (a, b) => new Date(a.publicationDate) - new Date(b.publicationDate);
+    pages.sort(byDateAsc);
+
+    let pageIndex = pages.findIndex(p => p.fullQualifiedURL === page.fullQualifiedURL);
+    
+    if (pageIndex > 0) {
+        let previousPage = pages[pageIndex - 1];
+        let document = templateDom.window.document;
+        let slot = document.querySelector(targetSelector) || pluginElement;
+
+        let previous = document.createElement("a");
+        let href = new URL(previousPage.fullQualifiedURL).pathname;
+        previous.href = href;
+        previous.innerHTML = linkText || previousPage.title;
+
+        slot.appendChild(previous);
+    } else {
+        // The first page has no predecessor
+    }
+
+    pluginElement.remove();
+}
