0d4ad57ceb0de39cd63444cf2a434a6ae53a45dd
Fabian Holzer <fabian.holzer.lists@gmail.com>
2024-04-28 14:06:57 +0200

    Add rudimentary watch mode

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ import { createPages } from "./core/create-pages.js"
 import { generateModel, addBacklinks } from "./core/create-model.js";
 import { traverse } from "./core/traverse.js"
 
+import Watcher from 'watcher';
+
 async function main() {
-    const [inputDirectory, outputDirectory, isDryRun] = process.argv.slice(2)
+
+    let commandLineArgs = process.argv.slice(2);
+    const [inputDirectory, outputDirectory, command] = commandLineArgs;
 
     const globalPropertiesFile =
         path.resolve(__dirname, inputDirectory, "intertwingle.json");
@@ -26,28 +30,75 @@ async function main() {
         return;
     }
 
-    let dryRun = (isDryRun === "--dry-run");
+    let dryRun = (command === "--dry-run");
+    let isWatchMode = (command === "watch");
 
-    const createModel = (file) => generateModel(
-        inputDirectory,
-        outputDirectory,
-        file,
-        globalProperties.url
-    );
 
-    let pages = await traverse(
-        inputDirectory,
-        createModel,
-    );
-    let model = { pages, globalProperties }
-    addBacklinks(model);
+    let generateWebsite = async () => {
+        console.log("(re)-generating website")
 
-    if (dryRun) {
-        console.log("Dry run, only builds models, but doesn't create output");
-        return;
+        const createModel = (file) => generateModel(
+            inputDirectory,
+            outputDirectory,
+            file,
+            globalProperties.url
+        );
+
+        let pages = await traverse(
+            inputDirectory,
+            createModel,
+        );
+        let model = { pages, globalProperties }
+        addBacklinks(model);
+
+        if (dryRun) {
+            console.log("Dry run, only builds models, but doesn't create output");
+            return;
+        }
+
+        await createPages(model);
+
+        initialGenerationComplete = true;
     }
 
-    await createPages(model);
+    let initialGenerationComplete = false;
+
+    if (isWatchMode) {
+        let ignore = (watchedPath) => {
+            let isIgnored = [
+                path.join(inputDirectory, ".git"),
+                path.join(inputDirectory, "node_modules"),
+                outputDirectory
+            ].some(ignored => watchedPath.startsWith(ignored));
+
+            return isIgnored;
+        }
+
+        const watcher = new Watcher(inputDirectory, {
+            ignore,
+            recursive: true,
+            native: false,
+        });
+
+        watcher.on("all", (event, targetPath, targetPathNext) => {
+            // console.log({ event, targetPath, targetPathNext });
+
+            if (initialGenerationComplete) {
+                generateWebsite();
+            } else {
+                console.log("site (re-)generation currently in progress...", targetPath)
+            }
+
+
+            // generateWebsite();
+        });
+    }
+
+    await generateWebsite();
+
+    if (isWatchMode) {
+        console.log("Initial generation completed. Watching for changes...")
+    }
 }
 
 await main();
