95bce2b13d667b5503c30fcd8d4f49d92c4ac9d3
Fabian Holzer <fabian.holzer.lists@gmail.com>
2024-05-07 21:47:03 +0200

    Add plugin for link to next page in category

---
+++ plugins/next-page.js
@@ -0,0 +1,42 @@
+export default async function addNextPage({
+    templateDom,
+    page,
+    metamodel,
+    pluginParams,
+    pluginElement,
+}) {
+
+    let targetSelector = pluginParams.target || "article";
+    let linkText = pluginParams["link-text"];
+
+
+    let pages = metamodel.pages.filter(p => p.category === page.category && p.isPublished && p.publicationDate);
+
+    let byDateAsc = (a, b) => new Date(a.publicationDate) - new Date(b.publicationDate);
+    pages.sort(byDateAsc);
+
+    let pageIndex = pages.findIndex(p => p.fullQualifiedURL === page.fullQualifiedURL);
+
+    if ((pageIndex + 1) < pages.length) {
+        let nextPage = pages[pageIndex + 1];
+        let document = templateDom.window.document;
+        let slot = document.querySelector(targetSelector);
+
+        if (!slot) {
+            console.log("No slot, using parent element of intertwingle plugin");
+            slot = pluginElement.parentElement;
+        }
+
+        let next = document.createElement("a");
+        let href = new URL(nextPage.fullQualifiedURL).pathname;
+        next.href = href;
+        next.innerHTML = linkText || `${nextPage.title}`;
+
+        slot.appendChild(next);
+    } else {
+        console.log("This is the last page it has no succesor");
+    }
+
+
+    pluginElement.remove();
+}
